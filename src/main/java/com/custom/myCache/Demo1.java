0324da5c16f012bfd839d5167d85a3a7a47c19b9
rahulreddy <v.rahul1719@gmail.com>
2018-07-05 09:01:21 +0530

    Code cleanup

--- src/main/java/com/custom/myCache/Demo1.java
+++ src/main/java/com/custom/myCache/Demo1.java
@@ -9,55 +9,49 @@ import java.util.List;
 
 
 public class Demo1 {
-    static int cacheHit=0;
+    static int cacheHit = 0;
+
     public static void main(String[] args) {
 
         CustomCache cache = new CustomCache(3);
-        Vehicle vehicle = null;
+        //Vehicle vehicle = null;
         DBHelper helper = new DBHelper();
         long start = System.currentTimeMillis();
         System.out.println("Start");
-        List ids= new ArrayList();
-        ids=createTestRequest();
+        List ids = new ArrayList();
+        ids = createTestRequest();
 
-        for (int i = 0; i < ids.size(); i++) {
-            boolean isCacheEnabled=true;
-            if (cache.get(ids.get(i))!=null  && isCacheEnabled) {
+        ids.forEach(id -> {
+            Vehicle vehicle = null;
+            System.out.println("From lambda --->" + id);
+            boolean isCacheEnabled = true;
+            if (cache.get(id) != null && isCacheEnabled) {
                 System.out.println("Accessing from cache");
-                vehicle = (Vehicle) cache.get(ids.get(i));
+                vehicle = (Vehicle) cache.get(id);
                 cacheHit++;
-            }
-            /*else if(cache.mruCache.get(ids.get(i))!=null && isCacheEnabled){
-                System.out.println("Accessing from L2 cache");
-                vehicle = (Vehicle) cache.mruCache.get(ids.get(i));
-                cacheHit++;
-            }*/
-            else {
+            } else {
                 System.out.println("Accessing from db");
-                vehicle = helper.getVehicleInfoById((Integer) ids.get(i));
-                cache.put(Integer.valueOf((Integer) ids.get(i)), vehicle);
+                vehicle = helper.getVehicleInfoById((Integer) id);
+                cache.put(Integer.valueOf((Integer) id), vehicle);
             }
-
-
             long endItr = System.currentTimeMillis();
             long total = endItr - start;
-            //System.out.println("End");
-            System.out.println("Total time for each itr  == "+ i +"-->"+ total);
-            //System.out.println("Cache size==" + lruCache.size());
-            System.out.println("Total cache hit === "+cacheHit);
-            System.out.println(vehicle.getId() + "---" + vehicle.getMake() + "---" + vehicle.getModel() + "---" + vehicle.getYear() + "---" + vehicle.getDrive());
 
-        }
+            System.out.println("Total time for each itr  -->" + total);
+            System.out.println("Total cache hit === " + cacheHit);
+            System.out.println(vehicle.getId() + "---" + vehicle.getMake() + "---" + vehicle.getModel() + "---" + vehicle.getYear() + "---" + vehicle.getDrive());
+        });
+
         long end = System.currentTimeMillis();
         long total = end - start;
         System.out.println("End");
-        System.out.println("Total time ===>"+ total);
+        System.out.println("Total time ===>" + total);
 
     }
 
     private static List createTestRequest() {
         TestVehicles testVehicles = new TestVehicles();
-    return testVehicles.vehicleIds;
+        return testVehicles.vehicleIds;
     }
 }
 
