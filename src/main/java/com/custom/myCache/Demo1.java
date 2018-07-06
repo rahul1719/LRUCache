500cd86c73c2a7b11b7f837bf9127ca9be7a6199
rahulreddy <v.rahul1719@gmail.com>
2018-07-06 10:02:32 +0530

    Code cleanup

--- src/main/java/com/custom/myCache/Demo1.java
+++ src/main/java/com/custom/myCache/Demo1.java
@@ -6,21 +6,38 @@ import com.custom.cache.Vehicle;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
 
 
-public class Demo1 {
+public class Demo1 implements Runnable {
     static int cacheHit = 0;
 
     public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        IntStream.range(1,10).parallel().forEach(i->{
+            try{
+                Runnable worker = new Demo1();
+                executor.execute(worker);
+            }
+            catch (Exception e){
+                System.out.println("Exception at " + i);
+            }
 
+        });
+        executor.shutdown();
+        while (!executor.isTerminated()) {
+        }
+        System.out.println("Finished all threads");
+
+    }
+
+    private static void getVehicleInfo(List ids) {
         CustomCache cache = new CustomCache(3);
-        //Vehicle vehicle = null;
         DBHelper helper = new DBHelper();
         long start = System.currentTimeMillis();
         System.out.println("Start");
-        List ids = new ArrayList();
-        ids = createTestRequest();
-
         ids.forEach(id -> {
             Vehicle vehicle = null;
             System.out.println("From lambda --->" + id);
@@ -36,22 +53,28 @@ public class Demo1 {
             }
             long endItr = System.currentTimeMillis();
             long total = endItr - start;
-
             System.out.println("Total time for each itr  -->" + total);
             System.out.println("Total cache hit === " + cacheHit);
             System.out.println(vehicle.getId() + "---" + vehicle.getMake() + "---" + vehicle.getModel() + "---" + vehicle.getYear() + "---" + vehicle.getDrive());
         });
-
         long end = System.currentTimeMillis();
         long total = end - start;
         System.out.println("End");
         System.out.println("Total time ===>" + total);
-
     }
 
     private static List createTestRequest() {
         TestVehicles testVehicles = new TestVehicles();
         return testVehicles.vehicleIds;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Thread started --> "+ Thread.currentThread().getName());
+        List ids = new ArrayList();
+        ids = createTestRequest();
+        getVehicleInfo(ids);
+        System.out.println("Thread end --> "+ Thread.currentThread().getName());
     }
 }
 
